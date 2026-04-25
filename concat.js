adb7df09bd2241da8bc8e1054a9a984e548393b4
Mehedi <mehedi.interface@gmail.com>
2026-04-25 09:06:45 +0600

    16_3 String Slice, Join, Concat and includes

--- concat.js
+++ concat.js
@@ -9,3 +9,6 @@ console.log(fullSentence);
 const name = "Alice";
 const greeting = "Hello, ".concat(name, "!"); 
 console.log(greeting);
+
+console.log(greeting.includes("Alice"));
+console.log(greeting.includes("Bob"));
